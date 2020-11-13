6a1ee5bf4c378ed65dd3e7b18d2cedfb3e4888be
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-13 22:00:05 +0000

    Automatically backed up by Learn

--- lib/your_solution.rb
+++ lib/your_solution.rb
@@ -1,4 +1,8 @@
 # Write your solution here
 
 # This file should output "Hello World!" using a puts statement
+<<<<<<< HEAD
 puts "Hello World!"
+=======
+puts "Hello World"
+>>>>>>> f0e6eecb80e8b14b41cce00075d14fc7b63c58c7
